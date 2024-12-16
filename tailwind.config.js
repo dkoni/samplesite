414dc611f73065e12de965d83019206078aaad0f
dkoni <dk834572@gmail.com>
2024-12-16 22:39:23 +0900

    firstcomit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      poppins: ['"Poppins"', 'sans-serif'],
+    },
+    extend: {
+      colors:{
+        body:"#050505",
+        "selected-text":"#A3A3FF",
+        theme:"#5c318c",
+        secondary:"#9191A4",
+        badge:"#3F3F51",
+        inputBorder:"#565666",
+        input:"#2A2A33"
+      },
+    },
+  },
+  plugins: [],
+}
+
